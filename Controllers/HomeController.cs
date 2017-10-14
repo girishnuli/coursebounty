24efab974090d5998afd2af9edb41fa398d5d7a8
Girish Nuli <>
2017-10-14 01:01:57 -0400

    Auth added with LiteDb

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using coursebounty.Models;
+using AspNetCore.Identity.LiteDB.Data;
+using LiteDB;
+
+namespace coursebounty.Controllers
+{
+    public class HomeController : Controller
+    {
+		private LiteDatabase _db { get; }
+
+		public HomeController(LiteDbContext dbContext)
+        {
+			_db = dbContext.LiteDatabase;
+		}
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using coursebounty.Models;
+using coursebounty.Services;
+using AspNetCore.Identity.LiteDB.Data;
+using AspNetCore.Identity.LiteDB;
+using LiteDbModels = AspNetCore.Identity.LiteDB.Models;
+using LiteDB;
+
+namespace coursebounty
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        public IHostingEnvironment _env { get; private set; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add LiteDB Dependency
+            services.AddSingleton<LiteDbContext>(db => 
+            new LiteDbContext(_env) {
+                LiteDatabase = new LiteDatabase(Configuration.GetConnectionString("DefaultConnection"))
+            });
+
+            services.AddIdentity<LiteDbModels.ApplicationUser, AspNetCore.Identity.LiteDB.IdentityRole>(options =>
+                            {
+                                options.Password.RequireDigit = false;
+                                options.Password.RequireUppercase = false;
+                                options.Password.RequireLowercase = false;
+                                options.Password.RequireNonAlphanumeric = false;
+                                options.Password.RequiredLength = 6;
+                            })
+            .AddUserStore<LiteDbUserStore<LiteDbModels.ApplicationUser>>()
+            .AddRoleStore<LiteDbRoleStore<AspNetCore.Identity.LiteDB.IdentityRole>>()
+            .AddDefaultTokenProviders();
+
+            services.AddTransient<IEmailSender, EmailSender>();
+
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            _env = env;
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseAuthentication();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
